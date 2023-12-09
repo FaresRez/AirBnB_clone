e34542292981c31a515e7894bccc75f314bbc7e7
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-12-09 09:36:26 +0000

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-import uuid
-from datetime import datetime
+import datetime
 
 class BaseModel:
-    
     def __init__(self):
-        BaseModel.id = str(uuid.uuid4())
-        BaseModel.created_at = datetime.now()
-        BaseModel.updated_at = datetime.now()
-    
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        self.id = uuid.uuid4()
 
+    def save(self):
+        self.updated_at = datetime.datetime.now()
 
-b= BaseModel()
-print(b.id)
+    def to_dict(self):
+        """Returns a dictionary representation of the BaseModel object.
 
-c= BaseModel()
-print(c.id)
+        Returns:
+            dict: A dictionary representation of the BaseModel object.
+        """
+        base_model_dict = self.__dict__
+        base_model_dict["updated_at"] = self.updated_at.isoformat()
+        base_model_dict["created_at"] = self.created_at.isoformat()
+        return base_model_dict
