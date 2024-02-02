cd3bbaf8c699e0c0a1bdab9f5781489ca335693d
FaresRez <fers.rez16@gmail.com>
2024-02-02 14:24:43 +0100

    Remove the test lines

--- models/base_model.py
+++ models/base_model.py
@@ -22,15 +22,3 @@ class BaseModel:
         new_dict = dict(self.__dict__)
         new_dict["__class__"] = self.__class__.__name__
         return  new_dict
-        
-
-
-
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
